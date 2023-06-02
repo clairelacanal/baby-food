ec5771753baf3d335581454428f15af26aee8b3f
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-02 10:42:19 +0200

    Accueil on going

--- src/Components/Accueil/Accueil.js
+++ src/Components/Accueil/Accueil.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import BurgerMenu from './Components/MenuBurger/MenuBurger';
+import Navbar from './Components/Navbar/Navbar';
 
 const Accueil = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleWindowResize);
+
+    // Clean up this component by removing the event listener when the component unmounts
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, 
+  []);
+
   return (
     <div>
+        {windowWidth > 768 ? <Navbar /> : <BurgerMenu />}
       <h1>Bienvenue sur notre site!</h1>
       <p>Ceci est la page d'accueil.</p>
     </div>
