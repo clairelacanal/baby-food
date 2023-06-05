5b618b3f32661697e9d6313191344490bb30160c
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-05 23:43:51 +0200

    login done

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import Accueil from './Components/Accueil/Accueil';
 import LargeButton from './Components/Buttons/Button';
 import LoginForm from './Components/Auth/Login/Login';
+import CreateAccountForm from './Components/Auth/Signup/Signup';
 
 
 function App() {
   return (
     <Router>
         <div className="App">
-          <Link to="/creation-du-compte"><LargeButton/></Link>
+          <Link to="/connexion"><LargeButton/></Link>
         </div>
         <Routes>
           <Route path="/" element={<Accueil/>}/>
-          <Route path="/creation-du-compte" element={<LoginForm/>}/>
+          <Route path="/connexion" element={<LoginForm/>}/>
+          <Route path="/creation-du-compte" element={<CreateAccountForm/>}/>
         </Routes>
     </Router>
   );
--- src/Components/Auth/Login/Login.js
+++ src/Components/Auth/Login/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({
@@ -60,7 +61,8 @@ const LoginForm = () => {
         </div>
         <button type="submit">Log In</button>
       </form>
-      <p>{`Vous n'avez pas encore de compte ? Inscrivez-vous`}</p> 
+      {`Vous n'avez pas encore de compte ? `}
+      <Link to="/creation-du-compte">Inscrivez-vous</Link> 
     </div>
   );
 };
