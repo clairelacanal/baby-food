78ae4ed230931995dec4cee9310ee17a74c3e44e
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-07 23:27:09 +0200

    starRating on going

--- src/Components/Recettes/Components/CartesPots/CartesPots.js
+++ src/Components/Recettes/Components/CartesPots/CartesPots.js
@@ -3,18 +3,17 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './CartesPots.scss';
 import recette1 from '../../../../Images/recipebaby-1.jpg';
 import React from 'react';
+import StarRating from '../Etoiles/Etoiles';
 
 const CardPots = () => {
   return (
     <div className="card-container">
-      <h6>Les nouvelles recettes de petits pots</h6>
+      <h6>Dernières recettes de petits pots</h6>
       <div className="card">
         <img src={recette1} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-          </p>
+          <StarRating/>
           <a href="#" className="btn btn-primary">
             Go somewhere
           </a>
---
+++ src/Components/Recettes/Components/Etoiles/Etoiles.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const StarRating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        
+        return (
+          <label key={i}>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <i
+              className="fas fa-star"
+              style={{ color: ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9" }}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(0)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+
+export default StarRating;
