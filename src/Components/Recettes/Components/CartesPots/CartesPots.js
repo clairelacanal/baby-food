132ec0479d142754e72cea9851f5747008045b64
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-07 15:53:09 +0200

    style card pots modified

--- src/Components/Recettes/Components/CartesPots/CartesPots.js
+++ src/Components/Recettes/Components/CartesPots/CartesPots.js
@@ -1,27 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './CardPots.scss';
 import recette1 from '../../../../Images/recipebaby-1.jpg';
-
 import React from 'react';
 
 const CardPots = () => {
   return (
-    <div>
-    <h6>Les nouvelles recettes de petits pots</h6>
-     <div className="card" style={{ width: '12rem' }}>
-       <img src={recette1} className="card-img-top" alt="..." />
-       <div className="card-body">
-         <h5 className="card-title">Card title</h5>
-         <p className="card-text">
-           Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-         </p>
+    <div className="card-container">
+      <h6>Les nouvelles recettes de petits pots</h6>
+      <div className="card">
+        <img src={recette1} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
+          </p>
           <a href="#" className="btn btn-primary">
-           Go somewhere
-         </a>
-       </div>
-     </div>
- </div>
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default CardPots;
+
