406ee36256b8dfd795ef8b6fe65d2578fdbc85a8
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-10 22:54:37 +0200

    page DME done

--- src/Components/Recettes/Components/CartesDME/CartesDme.js
+++ src/Components/Recettes/Components/CartesDME/CartesDme.js
@@ -1,26 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
 import React from 'react';
+import recette2 from '../../../../Images/recipebaby-2.jpg';
+import StarRating from '../Etoiles/Etoiles';
 
 const CardDme = () => {
   return (
-    <div>
-    <h6>Les nouvelles recettes de DME</h6>
-     <div className="card" style={{ width: '18rem' }}>
-       <img src="..." className="card-img-top" alt="..." />
-       <div className="card-body">
-         <h5 className="card-title">DME : nos nouvelles recettes pour encourager l&apos;autonomie de bébé</h5>
-         <p className="card-text">
-           Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-         </p>
+    <div className="card-container">
+      <h6>Les nouvelles recettes de DME</h6>
+      <div className="card">
+        <img src={recette2} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <StarRating/>
           <a href="#" className="btn btn-primary">
-           Go somewhere
-         </a>
-       </div>
-     </div>
- </div>
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </div>
   );
-};
+  };
 
 export default CardDme;
