3cfa8aa37b91ecb2d6831a9e1983e4f3729e1373
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-10 23:15:33 +0200

    bandeau vegetables on going

--- src/Components/Accessoires/FruitsLegumesSaison/FruitsLegumesSaison.js
+++ src/Components/Accessoires/FruitsLegumesSaison/FruitsLegumesSaison.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import vegetables from '../../../Images/vegetables.jpg';
 
 const FruitsVegetables = () => {
   return (
     <div className="card text-bg-dark">
-      <img src="..." className="card-img" alt="..." />
+      <img src={vegetables} className="card-img" alt="..."/>
       <div className="card-img-overlay">
         <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small>Last updated 3 mins ago</small></p>
       </div>
     </div>
   );
 };
-
 
 export default FruitsVegetables;
