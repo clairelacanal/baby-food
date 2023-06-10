8f65a40dbe2b2af68c78bfb638fdfe8693203794
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-10 23:26:12 +0200

    jumbotton on  going

--- src/Components/Accessoires/FruitsLegumesSaison/FruitsLegumesSaison.js
+++ src/Components/Accessoires/FruitsLegumesSaison/FruitsLegumesSaison.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import vegetables from '../../../Images/vegetables.jpg';
+import './FruitsLegumesSaison.scss';  // Ajoutez cette ligne pour importer les styles SCSS
 
 const FruitsVegetables = () => {
   return (
     <div className="card text-bg-dark">
       <img src={vegetables} className="card-img" alt="..."/>
       <div className="card-img-overlay">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">Les légumes & fruits de saison</h5>
       </div>
     </div>
   );
