5f052eca21be601e625bde4e976018eb45ea206c
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-14 14:37:06 +0200

    cook kids done

--- src/Components/Accessoires/Accessoires.js
+++ src/Components/Accessoires/Accessoires.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import image from '../../Images/cook-kids.jpg';
+import './Accessoires.scss';
 
 const Accessories = () => {
   return (
     <div className="card text-bg-dark">
-      <img src="..." className="card-img" alt="..." />
+      <img src={image} className="card-img" alt="..." />
       <div className="card-img-overlay">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p className="card-text"><small>Last updated 3 mins ago</small></p>
+        <h5 className="card-title">Les accessoires de cuisine</h5>
       </div>
     </div>
   );
