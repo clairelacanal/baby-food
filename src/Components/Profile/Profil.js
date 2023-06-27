87795dda1a6df2b2fd851a8a7de9969ac16e6755
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-27 11:21:22 +0200

    profil v2

--- src/Components/Profile/Profil.js
+++ src/Components/Profile/Profil.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faSpoon, faUsers } from '@fortawesome/free-solid-svg-icons';
+import 'materialize-css/dist/css/materialize.min.css';
+import imageAvatar from '../../Images/image-avatar.jpg';
 
 const Profil = () => {
     return (
-        <div className="container-profil">
-            <div className="container-user">
-            <img>
-
-            </img>
-            <div className="container-userName">
-                <h3>John Doe</h3>
-                <h6>Bienvenue sur votre profil</h6>
-                <p>Mes recettes préférées</p>
-                <p>Mes recettes partagées</p>
-                <p>Mes aliments testés</p>
-            </div>
-            </div>
+    <div className="container">
+    <ul>
+      <li className="collection-item avatar">
+      <img src={imageAvatar} alt="" className="circle" style={{width: '60px', height: '60px'}} />
+        <span className="title">John Doe</span>
+        <br/>
+        <span>Bienvenue sur votre profil</span>
+        <p><FontAwesomeIcon icon={faHeart}/>Mes recettes préférées</p>
+        <br/>
+        <p><FontAwesomeIcon icon={faUsers}/>Mes recettes partagées</p>
+        <br/>
+        <p><FontAwesomeIcon icon={faSpoon}/>Mes aliments testés</p>
+      </li>
+    </ul>
         </div>
     )
 }
