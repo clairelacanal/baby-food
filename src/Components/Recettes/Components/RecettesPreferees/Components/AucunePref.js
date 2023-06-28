76d67ce69b237c95c305316088fb758d81be04c5
Claire Lacanal <lacanal.claire@gmail.com>
2023-06-28 15:04:13 +0200

    recipes like on going v1

---
+++ src/Components/Recettes/Components/RecettesPreferees/Components/AucunePref.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const AucuneRecettePreferee = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
+  return (
+    <div>
+      <p>Vous n’avez pas encore de recettes préférées</p>
+      <input 
+        type="search" 
+        placeholder="Chercher une recette" 
+        value={searchTerm} 
+        onChange={handleSearchChange}
+      />
+    </div>
+  );
+}
+
+export default AucuneRecettePreferee;
