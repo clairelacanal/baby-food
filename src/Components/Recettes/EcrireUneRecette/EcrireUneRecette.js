d3ee103ab46e3c8b6b3fd425643971c0ac7a831d
Claire Lacanal <lacanal.claire@gmail.com>
2023-09-12 15:07:49 +0200

    ecrire une recette on going

--- src/Components/Recettes/EcrireUneRecette/EcrireUneRecette.js
+++ src/Components/Recettes/EcrireUneRecette/EcrireUneRecette.js
@@ -6,7 +6,7 @@ class EcrireUneRecette extends Component {
     super(props);
     this.state = {
       title: '',
-      isDme: true, // Initialiser isDme dans l'état
+      isDme: true,
       photo: null,
       prepTime: '',
       cookTime: '',
@@ -28,15 +28,9 @@ class EcrireUneRecette extends Component {
     });
   }
 
- /* handleArrayChange = (event, index, arrayName) => {
-    const array = this.state[arrayName].slice();
-    array[index] = event.target.value;
-    this.setState({ [arrayName]: array });
-  }*/
-
   handleIsDmeChange = (event) => {
     this.setState({
-      isDme: event.target.value === 'true', // Convertir la valeur du bouton radio en booléen
+      isDme: event.target.value === 'true',
     });
   }
 
@@ -46,62 +40,57 @@ class EcrireUneRecette extends Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Ecrire une recette</h1>
-      <div className='div-input-radio'>
-        <label className='form-label'>
-          DME:
-          <input 
-            type="radio" 
-            name="isDme" 
-            value="true"
-            checked={this.state.isDme === true}
-            onChange={this.handleIsDmeChange}
-            className='radio-input'
-          />
-        </label>
-        <label className='form-label'>
-          PETITS POTS:
-          <input 
-            type="radio" 
-            name="isDme" 
-            value="false"
-            checked={this.state.isDme === false}
-            onChange={this.handleIsDmeChange}
-            className='radio-input'
-          />
-        </label>
+      <form onSubmit={this.handleSubmit} className='form-container'>
+        <h1 className='form-title'>Ecrire une recette</h1>
+        <div className='div-input-radio'>
+          <label className='form-label'>
+            DME:
+            <input 
+              type="radio" 
+              name="isDme" 
+              value="true"
+              checked={this.state.isDme === true}
+              onChange={this.handleIsDmeChange}
+              className='form-radio'
+            />
+          </label>
+          <label className='form-label'>
+            PETITS POTS:
+            <input 
+              type="radio" 
+              name="isDme" 
+              value="false"
+              checked={this.state.isDme === false}
+              onChange={this.handleIsDmeChange}
+              className='form-radio'
+            />
+          </label>
         </div>
         <label className='form-label'>
-          Proposez un titre:
-          <input type="text" name="title" onChange={this.handleInputChange} className="form-input" />
+          <input type="text" name="title" onChange={this.handleInputChange} className='form-input' placeholder='Titre de la recette' />
         </label>
         <label className='form-label'>
-          Téléchargez une photo:
-          <input type="file" name="photo" onChange={this.handleFileChange} className='form-input' />
+          <input type="file" name="photo" onChange={this.handleFileChange} className='form-input' placeholder='Téléchargez une photo' />
         </label>
         <label className='form-label'>
-          Temps de préparation:
-          <input type="text" name="prepTime" onChange={this.handleInputChange} className='form-input' />
+          <input type="text" name="prepTime" onChange={this.handleInputChange} className='form-input' placeholder='Temps de préparation' />
         </label>
         <label className='form-label'>
-          Temps de cuisson:
-          <input type="text" name="cookTime" onChange={this.handleInputChange} className='form-input' />
+          <input type="text" name="cookTime" onChange={this.handleInputChange} className='form-input' placeholder='Temps de cuisson' />
         </label>
         <label className='form-label'>
-          Indiquez l’âge de l’enfant:
-          <input type="text" name="childAge" onChange={this.handleInputChange} className='form-input'/>
+          <input type="text" name="childAge" onChange={this.handleInputChange} className='form-input' placeholder="L'âge de l'enfant"/>
         </label>
-        {['Ingrédient 1', 'Ingrédient 2', 'Ingrédient 3'].map((label, i) => (
+        {['Ingrédient 1'].map((label, i) => (
           <label key={i} className='form-label'>
             {label}:
-            <input type="text" onChange={(e) => this.handleArrayChange(e, i, 'ingredients')} className='form-input' />
+            <input type="text" onChange={(e) => this.handleArrayChange(e, i, 'ingredients')} className='form-input' placeholder='Un ingrédient' />
           </label>
         ))}
-        {['Tâche 1', 'Tâche 2', 'Tâche 3'].map((label, i) => (
+        {['Tâche 1'].map((label, i) => (
           <label key={i} className='form-label'>
             {label}:
-            <input type="text" onChange={(e) => this.handleArrayChange(e, i, 'tasks')} className='form-input' />
+            <input type="text" onChange={(e) => this.handleArrayChange(e, i, 'tasks')} className='form-input' placeholder='Une tâche' />
           </label>
         ))}
         <input type="submit" value="Validez" />
@@ -110,5 +99,5 @@ class EcrireUneRecette extends Component {
   }
 }
 
-
 export default EcrireUneRecette;
+
